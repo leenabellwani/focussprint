1124d2f8cfdda0c5e4fb0a78006a6811d5fec421
Leena Bellwani <leena.bellwani@gmail.com>
2026-06-28 20:39:56 +0530

    warnings handled

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -18,4 +18,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
